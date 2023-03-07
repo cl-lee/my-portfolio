60133dd46f8ced121816c303950d3c867a03acc2
cl.lee <cl.lee.ofz@gmail.com>
2023-03-07 22:21:31 +0000

    removed pseudocodes in Project.js

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,25 +1,7 @@
 import React from "react";
-// probably doesn't belong to this component
-
-// Add Router here
-// Add Route paths to all projects?
-
-// user select -> setState
-
-// function
-
-// props: which index of array?
-
-
 
 export default function Project(props) {
   return (
-    // <Router basename="project-gallery">
-    // {/* drop down menu to select project here */}
-    //     <Routes>
-    //         <Route path="/" element={<renderProject />} />
-    //        </Routes>
-    // </Router>
       <section>
         <h1>{props.title}</h1>
         <p>
@@ -32,25 +14,3 @@ export default function Project(props) {
       </section>
     );
 }
-
-// function RenderProject(props) {
-//   return (
-//     <section>
-//       <h1>{props.title}</h1>
-//       <p>
-//         Deployed version: <a href={props.deployedLink}>{props.deployedLink}</a>
-//       </p>
-//       <p>
-//         GitHub repository: <a href={props.githubLink}>{props.githubLink}</a>
-//       </p>
-//       <img alt={props.title} src={props.image} />
-//     </section>
-//   );
-// }
-
-// drop down menu to select project
-// function SelectProject()
-
-
-// if user select project 1, route to project 1 page and render information
-// etc. for all projects
