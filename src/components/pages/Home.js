9a59d3022b43be74b141a7340e6b525b0aeddba2
cl.lee <cl.lee.ofz@gmail.com>
2023-03-10 10:51:13 +0000

    updated Home content texts, updated fonts for Home component

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,15 +4,27 @@ import "./styles/Home.css";
 // Renders HTML for home page
 function Home() {
   return (
-    <div>
+    <div className="p-4">
       <h1>Chung Lai Lee</h1>
       <h2>Front End Web Developer</h2>
-      <h3>I am a front end web developer with an interest in problem solving. I love to build apps that makes life a bit more fun and easy for everyone!</h3>
+      <h3>
+        Welcome to my webpage! You can find information about me and my
+        projects here!
+      </h3>
       <p>
-        Welcome to my webpage! Here you can find information about me and my projects!
+        I am a front end web developer with an interest in problem solving. I
+        love to build apps that makes life a bit more fun and easier for everyone!
       </p>
       <p>
-        A copy of my CV can be found here: <a href="https://drive.google.com/file/d/1IZpGdNwzV9zR1Gp4nW3AcZO0-y6eAi7G/view?usp=sharing">My CV</a>
+        Skills: Working knowledge of HTML, CSS, Bootstrap, Javascript, ES6,
+        JQuery, Node.js, ReactJS, APIs, source control, test driven & behavior
+        driven development, and Visual Studio
+      </p>
+      <p>
+        CV: A copy of my CV can be found here:{" "}
+        <a href="https://drive.google.com/file/d/1IZpGdNwzV9zR1Gp4nW3AcZO0-y6eAi7G/view?usp=sharing">
+          My CV
+        </a>
       </p>
     </div>
   );
