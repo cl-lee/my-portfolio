a0fcbb2b93879cd8b05c60086cb0d342466bc2bb
cl.lee <cl.lee.ofz@gmail.com>
2023-03-16 15:04:42 +0000

    moved profile pic to navtabs

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,9 @@
 import React from "react";
 import NavTabs from "./NavTabs";
-import myPicture from "../assets/my-picture.JPG"
 
 function Header() {
   return (
     <div>
-      <img id="my-pic" src={myPicture} alt="Me on a pier in sunset"/>
       <NavTabs/>
     </div>
   );
--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import "./styles/NavTabs.css"
+import "./styles/NavTabs.css";
+import myPicture from "../assets/my-picture.JPG"
 
 // Renders navigation tabs on top of the page
 function NavTabs() {
   return (
     <ul className="nav nav-tabs p-2">
+      <img
+        id="my-pic"
+        src={myPicture}
+        width="30"
+        height="30"
+        alt="Me on a pier in sunset"
+      />
       <li className="nav-item col-3 col-lg-2 p-2">
         <NavLink
           to="/"
