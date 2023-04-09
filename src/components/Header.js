0502d3a41ba3874f483a969aa592371193fa79e0
cl.lee <cl.lee.ofz@gmail.com>
2023-04-09 18:59:41 +0100

    removed pseudocodes from NavTabs.js, moved profile pic from Header to NavTabs, updated NavTabs layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,11 @@
 import React from "react";
 import NavTabs from "./NavTabs";
-import myPicture from "../assets/my-picture.JPG"
 import "./styles/Header.css";
 
 function Header() {
   return (
     <div id="app-header">
-      <img id="my-pic" src={myPicture} alt="Me on a pier in sunset"/>
-      <NavTabs/>
+      <NavTabs />
     </div>
   );
 }
--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,20 +1,24 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import "./styles/NavTabs.css";
+import myPicture from "../assets/my-picture.JPG";
 
 // Renders navigation tabs on top of the page
 function NavTabs() {
   return (
-    <ul className="nav p-2 d-flex justify-content-start">
-      <li className="nav-item p-2">
+    <ul id="nav-tab" className="d-flex justify-content-start align-items-center">
+      <li className="no-bullets p-2">
+        <img id="my-pic" src={myPicture} alt="Me on a pier in sunset" />
+      </li>
+      <li className="no-bullets p-2">
         <NavLink to="/" end>
           Home
         </NavLink>
       </li>
-      <li className="nav-item p-2">
+      <li className="no-bullets p-2">
         <NavLink to="project-gallery">Project Gallery</NavLink>
       </li>
-      <li className="nav-item p-2">
+      <li className="no-bullets p-2">
         <NavLink to="contact">Contact</NavLink>
       </li>
     </ul>
@@ -22,16 +26,3 @@ function NavTabs() {
 }
 
 export default NavTabs;
-
-// show hamburger menu for smaller screen, just nav for bigger screens.
-// 1) hamburger menu:
-//    - <button>, svg file for hamburger image (saved to public/assets)</button>
-//    - show on default (small screens)
-//    - put on the right side (float?)
-//    - set state for hamburger menu, on click, display/hide navigation
-//    - when navigation items on display: flexbox? flex-direction: column
-//    - display below hamburger image
-// 
-// 2)  navigation items:
-//    - hide on default (small screens) (display: none)
-//    - media query on larger screens: @media (min-width: tbc) : display: flex, flex direction: row
