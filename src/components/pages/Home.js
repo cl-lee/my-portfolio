1827b588ada06c1d0a62973d4ae7bfb7db54a6ca
cl.lee <cl.lee.ofz@gmail.com>
2023-05-05 21:53:40 +0100

    CV links will now be opened in new tab

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -8,12 +8,13 @@ function Home() {
       <h1>Chung Lai Lee</h1>
       <h2>Front End Web Developer</h2>
       <h3>
-        Welcome to my webpage! You can find information about me and my
-        projects here!
+        Welcome to my webpage! You can find information about me and my projects
+        here!
       </h3>
       <p>
         I am a front end web developer with an interest in problem solving. I
-        love to build apps that makes life a bit more fun and easier for everyone!
+        love to build apps that makes life a bit more fun and easier for
+        everyone!
       </p>
       <p>
         I have working knowledge of HTML, CSS, Bootstrap, Javascript, ES6,
@@ -22,7 +23,11 @@ function Home() {
       </p>
       <p>
         CV: A copy of my CV can be found here:{" "}
-        <a href="https://drive.google.com/file/d/1N-8PDnGETQStxoMBqNvkpUWgkLHbGvua/view?usp=sharing">
+        <a
+          href="https://drive.google.com/file/d/1N-8PDnGETQStxoMBqNvkpUWgkLHbGvua/view?usp=sharing"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           My CV
         </a>
       </p>
